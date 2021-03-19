f6a9ae8f0e9f82b170b1295ea5e7a02ff7ce0afc
Justin Raver <justinraver113@gmail.com>
2021-03-19 11:04:10 -0600

    Added column creation when project is created

--- Github-HelperBot/index.js
+++ Github-HelperBot/index.js
@@ -7,7 +7,15 @@ module.exports = (app) => {
   app.log.info("Yay, the app was loaded!");
 
   app.on("project.created", async (context) =>{
+    let projId = context.payload.project.id;
+    const cols = ["New Issues", "Product Backlog (Stories)", "Sprint 0 Backlog (Stories)", "Sprint 0 Backlog (Tasks)", "In Progress", "Review/QA", "Closed"];
 
+    for (const col of cols) {
+      context.octokit.projects.createColumn({
+        project_id: projId,
+        name: col
+      });
+    }
   });
 
   app.on("issues.opened", async (context) => {
