2485bff723cfc5838011b7435dfeac3ee89f9b89
Justin Raver <48226937+JustinRaver@users.noreply.github.com>
2021-03-20 18:16:11 -0600

    Closes #3 (#6)
    * Added code to get the current repo labels
    
    * Added label ame and color list along with functionality to check current labels
    
    * Changed labels and got functionality working to add default labels
    
    * Final touches on label creation
    
    * Reformatting code
    
    * Deletes default labels
    
    Co-authored-by: Justin Raver <justinraver113@gmail.com>

--- Github-HelperBot/index.js
+++ Github-HelperBot/index.js
@@ -3,31 +3,66 @@
  * @param {import('probot').Probot} app
  */
 module.exports = (app) => {
-  // Your code here
-  app.log.info("Yay, the app was loaded!");
+    app.log.info("Yay, the app was loaded!");
 
-  app.on("project.created", async (context) =>{
-    let projId = context.payload.project.id;
-    const colNames = ["New Issues", "Product Backlog (Stories)", "Sprint 0 Backlog (Stories)", "Sprint 0 Backlog (Tasks)", "In Progress", "Review/QA", "Closed"];
-    //Add column for each name when project is created
-    for (const name of colNames) {
-      context.octokit.projects.createColumn({
-        project_id: projId,
-        name: name
-      });
-    }
-  });
+    app.on("installation_repositories.added", async (context) => {
+        const repos = context.payload.repositories_added;
+        const repoOwner = context.payload.sender.login;
 
-  app.on("issues.opened", async (context) => {
-    const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
+        for (const repo of repos) {
+            // Get all labels for the repository
+            let {data: labels} = await context.octokit.issues.listLabelsForRepo({
+                owner: repoOwner,
+                repo: repo.name
+            })
+
+            // Delete All default RepoLabels
+            labels.forEach(function (label) {
+              context.octokit.issues.deleteLabel({
+                owner: repoOwner,
+                repo: repo.name,
+                name: label.name,
+              });
+            })
+
+            //label names and colors *** both arrays depend are order dependent ***
+            const labelNames = ["documentation :page_facing_up:", "enhancement", "question :question:", "low-priority", "medium-priority", "high-priority :exclamation::exclamation:", "story :book:", "epic", "task", "bug :bug:"];
+            const labelColors = ["0075ca", "a2eeef", "d876e3", "2dc937", "e7b416", "ff6600", "355c7d", "1143a9", "a1e9ea", "d73a4a"];
+            //Make all the specified labels
+            for (let i = 0; i < labelColors.length; i++) {
+                    context.octokit.issues.createLabel({
+                        owner: repoOwner,
+                        repo: repo.name,
+                        name: labelNames[i],
+                        color: labelColors[i]
+                    });
+            }
+        }
     });
-    return context.octokit.issues.createComment(issueComment);
-  });
 
-  // For more information on building apps:
-  // https://probot.github.io/docs/
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+    app.on("project.created", async (context) => {
+        let projId = context.payload.project.id;
+        const colNames = ["New Issues", "Product Backlog (Stories)", "Sprint 0 Backlog (Stories)", "Sprint 0 Backlog (Tasks)", "In Progress", "Review/QA", "Closed"];
+        //Add column for each name when project is created
+        for (const name of colNames) {
+            context.octokit.projects.createColumn({
+                project_id: projId,
+                name: name
+            });
+        }
+    });
+
+    app.on("issues.opened", async (context) => {
+        const issueComment = context.issue({
+            body: "Thanks for opening this issue!",
+        });
+        return context.octokit.issues.createComment(issueComment);
+    });
+
+    // For more information on building apps:
+    // https://probot.github.io/docs/
+
+    // To get your app running against GitHub, see:
+    // https://probot.github.io/docs/development/
 };
